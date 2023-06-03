5d4ec2100bd6da618b34a8d53ca565d89cbad91a
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2023-06-03 01:36:07 -0700

    News 3 path refactored

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -127,7 +127,7 @@ const CarouselWithText = () => {
         <div className="relative w-screen h-500">
         <Link href="/team">
           <Image
-            src="/public/assets/News3.jpeg"
+            src="/assets/News3.jpeg"
             alt="Landing Page Image"
             width={1500}
             height={600}
