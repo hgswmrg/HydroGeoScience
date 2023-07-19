ebd9688113c7d7739617aad6dc93a27ea2deaffc
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2023-07-19 18:50:50 +0530

    Revert "revision to news page footer"
    This reverts commit 847c8b29cca60410d2b3bbcb71ef6295825ebec0.

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -28,14 +28,14 @@ export default function News() {
   };
 
   return (
-    <section className="w-full h-full 2xl:h-screen flex flex-col items-start mx-auto md:px-20 mb-20">
+    <section className="w-full h-full flex flex-col items-start mx-auto md:px-20 mb-20">
       <div className="mt-20 md:mt-40 w-full flex flex-col p-5">
         <p className="font-bold text-2xl md:text-5xl mb-5 text-primary-darkgreen">
           Latest News
         </p>
         
       </div>
-      <div className="flex md:mt-10 w-full h-full flex-col items-start">
+      <div className="flex md:mt-10 w-full  flex-col items-start">
           {newsData.map((newsItem, index) => (
             <div key="index"
             className={`news-card bg-primary-lightgreen w-full m-4 p-5 ${
