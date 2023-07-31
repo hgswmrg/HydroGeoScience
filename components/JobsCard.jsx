2c9ca993e85b3de512a9917d0a92564b8314721c
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2023-07-31 21:17:28 +0530

    rebooting for lfs support

--- components/JobsCard.jsx
+++ components/JobsCard.jsx
@@ -6,7 +6,7 @@ const JobsCard = ({jobsItem}) => {
     <div key={jobsItem._id} className="jobs-card bg-primary-lightgreen w-full md:m-4 mb-4 p-5">
         <h2 className="text-xl 2xl:text-2xl font-bold">{jobsItem.title}</h2>
         <p className="text-base 2xl:text-lg ">{jobsItem.description}</p>
-        <Link href={jobsItem.link} className="text-primary-darkgreen 2xl:text-xl underline mt-2">
+        <Link href={jobsItem.link} className="text-primary-darkgreen 2xl:text-xl underline mt-2 2xl:mt-2">
         Read More
         </Link>
   </div>
