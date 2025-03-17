eba08b3e1059ad620fc47aead3162b393e8d1052
Beta-test-initiation <tanyabansal2203@gmail.com>
2025-03-16 23:15:35 -0700

    modified publications, added work products

--- backend/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import news from './news';
-import jobs from './jobs';
-import profile from './profile';
-import publications from './publications';
-import carousel from './carousel';
-
-export const schemaTypes = [news, jobs, profile, publications, carousel ]
